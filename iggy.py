56453eeca38639c068d522faada8a332680d7eed
shaurobi <shaurobi@cisco.com>
2017-04-04 12:33:13 +1000

    Updated help message

--- iggy.py
+++ iggy.py
@@ -39,8 +39,7 @@ def buildmessage(in_message, webhook, person):
     if 'batman' in in_message or 'whoareyou' in in_message:
         msg = 'I\'m Batman!'
     elif 'help' in in_message:
-        msg = ("""Hi! I'm **Iggy the IoT Bot**.
-    Here's a list of things you can do -
+        msg = ("""Hi! I'm **Iggy the IoT Bot**. Here's a list of things you can do -
     Ask for the **chlorine** level to see what the tanks doing.
     You can also ask **who** is on duty and **start** a Webex meeting if needed.
     You can also request to view my **source code**
